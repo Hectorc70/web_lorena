bf103e7304cbcaae2ff47b86a4780c7dab67aff0
Hector-Home <hecgarciann@gmail.com>
2021-12-04 22:16:13 -0600

    dsds

--- web_project/settings/prod.py
+++ web_project/settings/prod.py
@@ -7,6 +7,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 ALLOWED_HOSTS = [os.getenv("DJANGO_ALLOWED_HOSTS"),
 'www.lorenacastrob.com',
 'lorenacastrob.com', 
+'web-lorena-d6o8t.ondigitalocean.app'
 ]
 
 DEBUG = os.getenv("DEBUG", "False") == "True"
